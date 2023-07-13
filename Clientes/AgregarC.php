e926e972bad2b449725b1130178458927165506f
Hamlet Simona <106114694+zeonBPM@users.noreply.github.com>
2023-07-13 16:09:15 -0300

    asd

--- Clientes/AgregarC.php
+++ Clientes/AgregarC.php
@@ -1,1 +1,11 @@
 agregar c
+
+
+Nombre: <input type="text" name="nombre" value="<?php echo $nombre;?>">
+
+Apellido: <input type="text" name="apellido" value="<?php echo $apellido;?>">
+
+Telefono: <input type="text" name="telefono" value="<?php echo $telefono;?>">
+
+Observaciones: <textarea name="observaciones" rows="5" cols="40"><?php echo $observacioones;?></textarea>
+
---
+++ index1 - Copy (4).php
@@ -0,0 +1,37 @@
+<?php
+echo"hola";
+	
+echo "ingresa tu nombre: ";
+
+
+echo "\n";
+
+
+
+
+
+
+
+
+switch($page)
+									{
+										case 'home':
+											include('./news.php');
+											break;
+
+										case 'downloads':
+											include('./downloads.php');
+											break;
+
+							
+									}
+
+
+
+
+
+
+
+
+
+?>
